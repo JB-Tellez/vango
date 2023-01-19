716b073f496081eb3a8347e55fb53f42decc9b4f
JB Tellez <jb.tellez@gmail.com>
2023-01-18 23:46:48 -0800

    adds env

--- vango_project/settings.py
+++ vango_project/settings.py
@@ -11,9 +11,27 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env(
+    # DEBUG=(bool, False),
+    # ENVIRONMENT=(str, "PRODUCTION"),
+    # ALLOW_ALL_ORIGINS=(bool, False),
+    # ALLOWED_HOSTS=(list, []),
+    # ALLOWED_ORIGINS=(list, []),
+    # CSRF_TRUSTED_ORIGINS=(list, []), # lab 39
+    DATABASE_ENGINE=(str, "django.db.backends.sqlite3"),
+    DATABASE_NAME=(str, BASE_DIR / "db.sqlite3"),
+    DATABASE_USER=(str, ""),
+    DATABASE_PASSWORD=(str, ""),
+    DATABASE_HOST=(str, ""),
+    DATABASE_PORT=(int, 5432),
+)
+
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -42,6 +60,7 @@ INSTALLED_APPS = [
 
     # 3rd party
     'compressor',
+    'environ',
 
     # local
     'vango_app',
@@ -83,8 +102,12 @@ WSGI_APPLICATION = 'vango_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": env.str("DATABASE_ENGINE"),
+        "NAME": env.str("DATABASE_NAME"),
+        "USER": env.str("DATABASE_USER"),
+        "PASSWORD": env.str("DATABASE_PASSWORD"),
+        "HOST": env.str("DATABASE_HOST"),
+        "PORT": env.int("DATABASE_PORT"),
     }
 }
 
